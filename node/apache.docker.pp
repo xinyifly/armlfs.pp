8f9684869e045b54c18e686a36529140b853166a
Zeyu Chen <zeyu.chen@btcc.com>
2016-03-19 18:06:19 +0800

    feat(apache): add proxy zhao,amber,test

--- node/apache.docker.pp
+++ node/apache.docker.pp
@@ -1,13 +1,43 @@
 node 'apache.docker' {
+
   class { 'apache':
     service_enable => false,
+    default_vhost => false,
   }
 
   apache::vhost { 'www.armlfs.org':
+    port => '80',
     docroot => '/var/www/html',
     proxy_pass => {
       'path' => '/',
-      'url' => 'http://192.168.42.1:17080',
+      'url' => 'http://192.168.42.1:17080/',
+    }
+  }
+
+  apache::vhost { 'zhao.armlfs.org':
+    port => '80',
+    docroot => '/var/www/html',
+    proxy_pass => {
+      'path' => '/',
+      'url' => 'http://192.168.42.1:18001/',
+    }
+  }
+
+  apache::vhost { 'amber.armlfs.org':
+    port => '80',
+    docroot => '/var/www/html',
+    proxy_pass => {
+      'path' => '/',
+      'url' => 'http://192.168.42.1:18011/',
+    }
+  }
+
+  apache::vhost { 'test.armlfs.org':
+    port => '80',
+    docroot => '/var/www/html',
+    proxy_pass => {
+      'path' => '/',
+      'url' => 'http://192.168.42.1:17081/',
     }
   }
 }
