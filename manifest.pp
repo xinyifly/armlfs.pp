774b91b81156997eed79e6559748a166ee6d25d8
Zeyu Chen <zeyu.chen@btcc.com>
2016-03-19 18:11:05 +0800

    refactor(node): add node/init.pp

--- manifest.pp
+++ manifest.pp
@@ -1,13 +1,5 @@
 import 'class.pp'
 
-import 'node/iZ11sgipzhjZ.pp'
-
-import 'node/mysql.docker.pp'
-
-import 'node/spigot.docker.pp'
-
-import 'node/apache.docker.pp'
-
-import 'node/mysql.dev.docker.pp'
+import 'node/init.pp'
 
 node default { }
---
+++ node/init.pp
@@ -0,0 +1,11 @@
+import 'node/iZ11sgipzhjZ.pp'
+
+import 'node/mysql.docker.pp'
+
+import 'node/spigot.docker.pp'
+
+import 'node/apache.docker.pp'
+
+import 'node/mysql.dev.docker.pp'
+
+import 'node/sun.dev.docker.pp'
